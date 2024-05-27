6c971b156d30132183e5620d4bbcf3178559438f
Sk. Nahid <nahidsheikh2001@gmail.com>
2024-05-27 22:26:04 +0600

    hash set done

--- LearnSet.java
+++ LearnSet.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class LearnSet {
@@ -20,6 +21,32 @@ public class LearnSet {
 
         System.out.println(set.contains(50));  //false; it will check for 50 in the array return will be boolean
 
+        System.out.println(set.size());
+        set.clear();
+        System.out.println(set);
 
+
+
+        //in LinkedHashSet data adds in a liked way means in a order way
+        Set<Integer> set1 = new LinkedHashSet<>();
+
+        set1.add(10);
+        set1.add(20);
+        set1.add(30);
+        set1.add(40);        
+        set1.add(50);
+        System.out.println(set1);  //[10, 20, 30, 40, 50]
+
+        set1.add(20); //trying to add duplicate value
+        System.out.println(set1);  //[10, 20, 30, 40, 50] its avoiding the duplicate value
+
+        set1.remove(50); //it will remove this value
+        System.out.println(set1); //[10, 20, 30, 40]
+
+        System.out.println(set1.contains(50));  //false; it will check for 50 in the array return will be boolean
+
+        System.out.println(set1.size());
+        set1.clear();
+        System.out.println(set1);
     }
 }
