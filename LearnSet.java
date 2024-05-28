de17522b9243d7557c3e4d917cfcac14cc72837f
Sk. Nahid <nahidsheikh2001@gmail.com>
2024-05-29 01:04:04 +0600

    hashcode using object class

--- LearnSet.java
+++ LearnSet.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class LearnSet {
     public static void main(String[] args) {
@@ -48,5 +49,44 @@ public class LearnSet {
         System.out.println(set1.size());
         set1.clear();
         System.out.println(set1);
+
+
+        //if we want to input the elements in sorted way we can use TreeSet() like->
+        Set<Integer> set2 = new TreeSet<>();
+
+        set2.add(20);
+        set2.add(40);
+        set2.add(10);
+        set2.add(30);        
+        set2.add(50);
+
+        System.out.println(set2);  //[10, 20, 30, 40, 50]
+
+        set2.add(20); //trying to add duplicate value
+        System.out.println(set2);  //[10, 20, 30, 40, 50] its avoiding the duplicate value
+
+        set2.remove(50); //it will remove this value
+        System.out.println(set2); //[10, 20, 30, 40]
+
+        System.out.println(set2.contains(50));  //false; it will check for 50 in the array return will be boolean
+
+        System.out.println(set2.size());
+        set2.clear();
+        System.out.println(set2);
+
+
+        //inserting duplicate value in HashSet using Object
+        Set<Student> studentsDetails = new HashSet<>();
+
+        studentsDetails.add(new Student("Sk. Nahid", 73));
+        studentsDetails.add(new Student("Rakib", 67));
+        studentsDetails.add(new Student("Fahim", 64));
+        studentsDetails.add(new Student("Sk. Nahid", 73));  //inserting duplicate value
+
+        System.err.println(studentsDetails); 
+        //output: [Student [Name=Sk. Nahid, rollNo=73], Student [Name=Fahim, rollNo=64], Student [Name=Sk. Nahid, rollNo=73], Student [Name=Rakib, rollNo=67]]
+
+        //it can be avoid using HashCode in Object Class => watch Student Object class to observe the hashcode(commented)
+        //[Student [Name=Rakib, rollNo=67], Student [Name=Sk. Nahid, rollNo=73], Student [Name=Fahim, rollNo=64]] - this is the new output which can filter duplicate value
     }
 }
---
+++ Student.java
@@ -0,0 +1,39 @@
+public class Student {
+    String Name;
+    int rollNo;
+
+    public Student(String Name, int rollNo){
+        this.Name = Name;
+        this.rollNo = rollNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [Name=" + Name + ", rollNo=" + rollNo + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + rollNo;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Student other = (Student) obj;
+        if (rollNo != other.rollNo)
+            return false;
+        return true;
+    }
+    
+
+    
+}   
